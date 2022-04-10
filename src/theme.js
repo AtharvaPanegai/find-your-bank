7af1ca1b1263f64ba23238033b11b4fc5d85b889
AtharvaPanegai <panegai.atharv@gmail.com>
2022-04-10 08:29:29 +0530

    Theme file for the app

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+// Create a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#6A1B4D",
+    },
+    secondary: {
+      main: "#6A1B4D",
+    },
+  },
+});
+
+export default theme;
